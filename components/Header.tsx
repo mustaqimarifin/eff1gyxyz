6e54c7bdbda671fb4252c94c0341899bd52e7457
vmp <xyz@xyz.local>
2023-03-23 07:45:42 +0800

    TRPC COMMENTS 14

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,13 +1,14 @@
-import { useCallback, useEffect, useRef } from "react";
-import Link from "next/link";
-import { BLOG } from "blog.config";
-import clsx from "clsx";
+import { useCallback, useEffect, useRef } from 'react';
+import Link from 'next/link';
+import { BLOG } from 'blog.config';
+import clsx from 'clsx';
+import Image from 'next/image';
 const NavBar = () => {
   const links = [
-    { id: 0, name: "Posts", to: BLOG.path || "/posts", show: true },
-    { id: 1, name: "About", to: "/about", show: BLOG.showAbout },
-    { id: 2, name: "Feed", to: "/feed", show: true },
-    { id: 3, name: "Search", to: "/search", show: true },
+    { id: 0, name: 'Posts', to: BLOG.path || '/posts', show: true },
+    { id: 1, name: 'About', to: '/about', show: BLOG.showAbout },
+    { id: 2, name: 'Feed', to: '/feed', show: true },
+    { id: 3, name: 'Search', to: '/search', show: true },
   ];
 
   return (
@@ -18,8 +19,7 @@ const NavBar = () => {
             link.show && (
               <li
                 key={link.id}
-                className="block ml-4 text-black dark:text-gray-50 nav"
-              >
+                className="block ml-4 text-black dark:text-gray-50 nav">
                 <Link href={link.to}>{link.name}</Link>
               </li>
             )
@@ -40,12 +40,12 @@ export const Header: React.FC<HeaderProps> = ({ navBarTitle, fullWidth }) => {
   const handler = useCallback(([entry]: IntersectionObserverEntry[]) => {
     if (navRef && navRef.current && !BLOG.autoCollapsedNavBar) {
       if (!entry.isIntersecting && entry !== undefined) {
-        navRef.current.classList.add("sticky-nav-full");
+        navRef.current.classList.add('sticky-nav-full');
       } else {
-        navRef.current.classList.remove("sticky-nav-full");
+        navRef.current.classList.remove('sticky-nav-full');
       }
     } else {
-      navRef?.current?.classList.add("remove-sticky");
+      navRef?.current?.classList.add('remove-sticky');
     }
   }, []);
   useEffect(() => {
@@ -61,19 +61,18 @@ export const Header: React.FC<HeaderProps> = ({ navBarTitle, fullWidth }) => {
       <div className="h-4 md:h-12" ref={sentinalRef}></div>
       <div
         className={clsx(
-          "sticky-nav m-auto w-full h-6 flex flex-row justify-between items-center mb-2 md:mb-12 py-8 bg-opacity-60",
+          'sticky-nav m-auto w-full h-6 flex flex-row justify-between items-center mb-2 md:mb-12 py-8 bg-opacity-60',
           {
-            "px-4 md:px-24": fullWidth,
-            "max-w-2xl px-4": !fullWidth,
+            'px-4 md:px-24': fullWidth,
+            'max-w-2xl px-4': !fullWidth,
           }
         )}
         id="sticky-nav"
-        ref={navRef}
-      >
+        ref={navRef}>
         <div className="flex items-center">
           <Link href="/" aria-label={BLOG.title}>
             <div className="min-w-max">
-              <b>yeehaw</b>
+              <Image src="/pw.png" alt="pw" width={16} height={16} />
             </div>
           </Link>
           {navBarTitle ? (
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,24 +1,67 @@
 // import Image from 'next/image';
-import Link from "next/link";
+import Link from 'next/link';
 
-import Container from "../components/Container";
+import Container from '../components/Container';
 export default function Home() {
   return (
     <Container>
       <div className="flex flex-col justify-center items-start max-w-5xl border-gray-200 dark:border-gray-700 mx-auto pb-16">
         <div className="flex flex-col-reverse sm:flex-row items-center">
-          <div className="flex flex-col pr-8">
-            <h1 className="font-bold text-3xl md:text-5xl tracking-tight mb-1 text-black dark:text-white xs:text-center">
-              Mustaqim Arifin
-            </h1>
-            <h2 className="text-gray-700 dark:text-gray-200 mb-4">
-              Head of Developer Relations at{" "}
-              <span className="font-semibold">Vercel</span>
-            </h2>
-            <p className="text-gray-600 dark:text-gray-400 mb-16">
-              Helping developers build a faster web. Teaching about web
-              development, serverless, and React / Next.js.
-            </p>
+          <div>
+            <div className="prose text-primary">
+
+              <p>
+                Hey, I&apos;m Mus. I&apos;m a{' '}
+                <a
+                  target="_blank"
+                  rel="noopener noreferrer"
+                  href="https://designdetails.fm">
+                  music producer
+                </a>
+                ,{' '}
+                <Link href="/writing" passHref>
+                  audio engineer
+                </Link>
+                , and{' '}
+                <a
+                  target="_blank"
+                  rel="noopener noreferrer"
+                  href="https://github.com/brianlovin">
+                  sorta terrible at code :D
+                </a>
+                . I&apos;m currently the marketing manager & creative lead at{' '}
+                <a
+                  target="_blank"
+                  rel="noopener noreferrer"
+                  href="https://github.com/mobile">
+                  musegroupasia
+                </a>
+                .
+              </p>
+              <p>
+                Before musegroupasia, I co-founded{' '}
+                <a
+                  target="_blank"
+                  rel="noopener noreferrer"
+                  href="https://spectrum.chat">
+                  Akhyla
+                </a>
+                , a platform to electronic musicians to learn, collaborate and
+                make records.
+              </p>
+              <p>
+                During those years I was also Music Director at Infinity Games
+                where I worked alongside some amazing talent helping shape the
+                sound and music of{' '}
+                <a
+                  target="_blank"
+                  rel="noopener noreferrer"
+                  href="https://buffer.com">
+                  Heroes of War
+                </a>
+                .
+              </p>
+            </div>
           </div>
         </div>
         <h3 className="font-bold text-2xl md:text-4xl tracking-tight mb-6 text-black dark:text-white">
@@ -44,15 +87,13 @@ export default function Home() {
         </div> */}
         <Link
           href="/posts"
-          className="flex mt-8 text-gray-600 dark:text-gray-400 leading-7 rounded-lg hover:text-gray-800 dark:hover:text-gray-200 transition-all h-6"
-        >
+          className="flex mt-8 text-gray-600 dark:text-gray-400 leading-7 rounded-lg hover:text-gray-800 dark:hover:text-gray-200 transition-all h-6">
           Read all posts
           <svg
             xmlns="http://www.w3.org/2000/svg"
             fill="none"
             viewBox="0 0 24 24"
-            className="h-6 w-6 ml-1"
-          >
+            className="h-6 w-6 ml-1">
             <path
               stroke="currentColor"
               strokeLinecap="round"
